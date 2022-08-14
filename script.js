c1e0baf9d6184b110c5aea519818c92e91f9e30b
meeky berry <meekyberry6@gmail.com>
2022-08-15 00:54:15 +0100

    Navigation hamburger fully added and page properly layout

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// const BUTTON = document.querySelector('.hamburger')
-// // const close = document.querySelector('.header__nav--close')
-// const NAV = document.querySelector('.header__nav')
-
-// BUTTON.addEventListener('click', () => {
-//     NAV.classList.add('header__nav')
-// })
-
-// // .addEventListener('click', () => {
-// //     nav.classList.remove('open-nav')
-// })
-
-const toggle = document.getElementById('toggle')
 const nav = document.getElementById('nav')
+const toggle = document.getElementById('toggle')
 
 toggle.addEventListener('click', () => nav.classList.toggle('active'))
