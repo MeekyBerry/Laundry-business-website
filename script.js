a77e021384be835b2f1568aee17564d088b94dec
meeky berry <meekyberry6@gmail.com>
2022-08-14 06:39:06 +0100

    hamburger function

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const menu = document.querySelector('.header__nav--menu')
-const close = document.querySelector('.header__nav--close')
-const nav = document.querySelector('.header__nav')
+// const BUTTON = document.querySelector('.hamburger')
+// // const close = document.querySelector('.header__nav--close')
+// const NAV = document.querySelector('.header__nav')
 
-menu.addEventListener('click', () => {
-    nav.classList.add('open-nav')
-})
+// BUTTON.addEventListener('click', () => {
+//     NAV.classList.add('header__nav')
+// })
 
-close.addEventListener('click', () => {
-    nav.classList.remove('open-nav')
-})
+// // .addEventListener('click', () => {
+// //     nav.classList.remove('open-nav')
+// })
+
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+toggle.addEventListener('click', () => nav.classList.toggle('active'))
