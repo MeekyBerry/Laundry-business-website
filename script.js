1a0fb3a2cd14a6561d52110d9ff301515dccf203
meeky berry <meekyberry6@gmail.com>
2022-08-22 19:58:28 +0100

    code update

--- script.js
+++ script.js
@@ -1,54 +1,12 @@
 /////  NAVIGATION BAR FUNCTION
-const nav = document.getElementById('nav')
-const toggle = document.getElementById('toggle')
+const NAV = document.getElementById('nav')
+const TOGGLE = document.getElementById('toggle')
 
-toggle.addEventListener('click', () => nav.classList.toggle('active'))
+TOGGLE.addEventListener('click', () => {
+  NAV.classList.toggle('active')
+})
 
 // ///// PAGE SCROLL IN ANIMATION FUNCTION
-// function reveal() {
-//   let reveals = document.querySelectorAll(".reveal");
-
-//   for (let i = 0; i < reveals.length; i++) {
-//     let windowHeight = window.innerHeight;
-//     let elementTop = reveals[i].getBoundingClientRect().top;
-//     let elementVisible = 150;
-
-//     if (elementTop < windowHeight - elementVisible) {
-//       reveals[i].classList.add("active");
-//     } else {
-//       reveals[i].classList.remove("active");
-//     }
-//   }
-// }
-
-// window.addEventListener("scroll", reveal);
-
-// // /// DARK MODE TOGGLE
-// // function myFunction() {
-// //   let toggleOn = document.body
-// //   toggleOn.classList.toggle("dark-mode");
-// // }
-
-/// NAVIGATION BAR SCROLL REVEAL
-let section = document.querySelectorAll("section");
-let menu = document.querySelectorAll("header nav a");
-
-window.onscroll = () => {
-  section.forEach((i) => {
-    let top = window.scrollY;
-    let offset = i.offsetTop - 150;
-    let height = i.offsetHeight;
-    let id = i.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      menu.forEach((link) => {
-        link.classList.remove("active");
-        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
-      });
-    }
-  });
-};
-
 function reveal() {
   let reveals = document.querySelectorAll(".reveal");
 
@@ -67,5 +25,49 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
-// To check the scroll position on page load
-reveal();
+// // /// DARK MODE TOGGLE
+// // function myFunction() {
+// //   let toggleOn = document.body
+// //   toggleOn.classList.toggle("dark-mode");
+// // }
+
+///  PAGE SCROLLING ANIMATION
+// let section = document.querySelectorAll("section");
+// let menu = document.querySelectorAll("header nav a");
+
+// window.onscroll = () => {
+//   section.forEach((i) => {
+//     let top = window.scrollY;
+//     let offset = i.offsetTop - 150;
+//     let height = i.offsetHeight;
+//     let id = i.getAttribute("id");
+
+//     if (top >= offset && top < offset + height) {
+//       menu.forEach((link) => {
+//         link.classList.remove("active");
+//         document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+//       });
+//     }
+//   });
+// };
+
+// function reveal() {
+//   let reveals = document.querySelectorAll(".reveal");
+
+//   for (let i = 0; i < reveals.length; i++) {
+//     let windowHeight = window.innerHeight;
+//     let elementTop = reveals[i].getBoundingClientRect().top;
+//     let elementVisible = 150;
+
+//     if (elementTop < windowHeight - elementVisible) {
+//       reveals[i].classList.add("active");
+//     } else {
+//       reveals[i].classList.remove("active");
+//     }
+//   }
+// }
+
+// window.addEventListener("scroll", reveal);
+
+// // To check the scroll position on page load
+// reveal();
